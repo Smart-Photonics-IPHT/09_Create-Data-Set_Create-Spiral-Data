4eadecb900ac132ca40563fef538f84a0818f026
Sobhi <saeed.sobhi@uni-jena.de>
2024-04-19 11:26:03 +0200

    Updated on 19-04-2024 Zero point was removed from the spirals

--- Create_Multiple_Spirals.py
+++ Create_Multiple_Spirals.py
@@ -1,30 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
-def generate_multiple_spirals(theta_max, totalpoints = 200, sprials_number = 4, max_noise=0):
+
+
+author = ""
+task = ""
+description = ""
+parameters = ""
+# Create main folder and sub folders and generate the readme file inside it
+# 
+
+def generate_multiple_spirals(theta_max, totalpoints=300, sprials_number=4, max_noise=0):
     #return a * np.exp(b * t) * np.array([np.cos(t), np.sin(t)])
-    t = np.linspace(0, theta_max, totalpoints)
+    step = theta_max*np.pi/totalpoints
+    t = np.linspace(step, theta_max*np.pi, totalpoints)
     Sprials = []
     for i in range(0, sprials_number):
-        Sprials.append(t * np.array([np.cos(t + i*2*np.pi/(sprials_number)) , np.sin(t + i*2*np.pi/(sprials_number))])
+        Sprials.append(t * np.array([np.cos(t + i*2*np.pi/(sprials_number)), np.sin(t + i*2*np.pi/(sprials_number))])
                        + np.random.uniform(-max_noise, max_noise, totalpoints))
     return t, Sprials[:sprials_number]
 
+Create_date = '040324'
+Create_date = 'Template'
+Create_date = '260324'
+Create_date = '100424'
+Create_date = 'Template'
+Create_date = '0.5piTemplate'
+Create_date = '150424'
 
-save_dir = r'C:\Sobhi\Results\spiral\Spiral_Data_Set'
+main_dir = r'C:\Sobhi\DataSet\Spiral_Data_Set\4_Spirals'
+main_dir = f'{main_dir}/{Create_date}'
 #for theta_max in (2*np.pi, 3*np.pi, 4*np.pi, 5*np.pi, 6*np.pi):
 #for theta_max in (6*np.pi):
-theta_max = 10*np.pi
+theta_max = 0.1 # 10pi
+#theta_max = 1
 #t, spiral1, spiral2 = generate_spiral(theta_max)
-for noise in (0.4, 0.6):
-    t, [spiral1, spiral2, spiral3, spiral4] = generate_multiple_spirals(theta_max, max_noise=noise)
+
+#for theta_max in (2, 4, 6, 8, 10, 12):
+# for denom in (0.01, 0.02, 0.05, 0.1, 0.5):
+#for denom in (2, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1):
+#for denom in (0.5, 1, 2, 4, 8, 16, 32):
+#for denom in (0.5, 1, 2, 4, 8, 16, 32):
+denom = 2
+denom_folder = f'{main_dir}/phase_factor_denom={denom}'
+if not os.path.exists(denom_folder):
+    os.makedirs(denom_folder)
+#new_folder = f'{main_dir}/theta_max={theta_max}'
+theta_folder = f'{denom_folder}/theta_max={theta_max}'
+theta_folder = f'{main_dir}/theta_max={theta_max}'
+if not os.path.exists(theta_folder):
+    os.makedirs(theta_folder)
+#for max_noise in (0, 0.1, 0.2, 0.4, 0.6, 0.8, 1, 1.2):
+for max_noise in (0, 0.1, 0.2):
+#for max_noise in (0, 1):
+    max_noise_folder = f'{theta_folder}/max_noise={max_noise}'
+    if not os.path.exists(max_noise_folder):
+        os.makedirs(max_noise_folder)
+    t, [spiral1, spiral2, spiral3, spiral4] = generate_multiple_spirals(theta_max, max_noise=max_noise)
 
     plt.figure(figsize=(16, 16))
     plt.scatter(spiral1[0], spiral1[1], s=50, label='Spiral 1', color='blue')
-    plt.scatter(spiral2[0], spiral2[1], s=50, label='Spiral 2', color='orange')
+    plt.scatter(spiral2[0], spiral2[1], s=50, label='Spiral 2', color='green')
     plt.scatter(spiral3[0], spiral3[1], s=50, label='Spiral 3', color='red')
-    plt.scatter(spiral4[0], spiral4[1], s=50, label='Spiral 4', color='purple')
+    plt.scatter(spiral4[0], spiral4[1], s=50, label='Spiral 4', color='black')
+
+    #this was used to show two points only that we know they were correctly classified
+    # plt.scatter(spiral1[0], spiral1[1], s=50,  color='blue', alpha=0.2)
+    # plt.scatter(spiral2[0], spiral2[1], s=50,  color='green', alpha=0.2)
+    # plt.scatter(spiral3[0], spiral3[1], s=50,  color='red', alpha=0.2)
+    # plt.scatter(spiral4[0], spiral4[1], s=50,  color='black', alpha=0.2)
+    #
+    # plt.scatter(-9.3, -28.022, s=50, label='Point from Spiral 1', color='blue')
+    # plt.scatter(-9.642, -29.568, s=50, label='Point from Spiral 4', color='black')
 
     #Set axis limits and add labels
     # plt.xlim([-19, 19])
@@ -32,13 +81,14 @@ for noise in (0.4, 0.6):
     plt.xlabel('X', fontsize=24)
     plt.ylabel('Y', fontsize=24)
     pi_symbol = '\u03c0'
-    plt.title(f'Theta_max =10{pi_symbol} _ Max_noise={noise}', fontsize=36)
+    plt.title(f'Theta_max ={theta_max}{pi_symbol} _ Max_noise={max_noise}', fontsize=24)
     plt.gca().set_aspect('equal')
 
     # Add a legend
     plt.legend(fontsize=14, loc='upper left')
     #plt.show()
-    plt.savefig(f'{save_dir}/Multiple_Spirals_theta_max=10pi_max_noise={noise}.png')
+    plt.savefig(f'{max_noise_folder}/Multiple_Spirals_theta_max={theta_max}pi_max_noise={max_noise}.png')
+    #plt.savefig(f'{max_noise_folder}/two_points_Multiple_Spirals_theta_max={theta_max}pi_max_noise={max_noise}.png')
     plt.close()
     # cnt += 1
     # Show the plot
@@ -50,7 +100,7 @@ for noise in (0.4, 0.6):
 
     #np.savetxt('Spiraldata.txt', np.hstack(combined_array1.flatten(), combined_array2.flatten()), delimiter='\t')
 
-    with open(f'{save_dir}/4_Spirals_data_theta_max=10pi_noise={noise}.txt', 'w') as file:
+    with open(f'{max_noise_folder}/4_Spirals_data_theta_max={theta_max}pi_noise={max_noise}.txt', 'w') as file:
         for x in combined_array1:
             file.write(f'1,{np.round(x[0],3)},{np.round(x[1],3)}\n')
         for y in combined_array2:
--- Create_Spirals_after_adding_noise.py
+++ Create_Spirals_after_adding_noise.py
@@ -1,57 +1,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#import scienceplots
+import os
 
+main_dir = r'C:\Sobhi\Results\spiral\Spiral_Data_Set\Two_Spirals'
 
-save_dir = r'C:\Sobhi\Results\spiral\Spiral_Data_Set'
-
-
-def generate_spiral(num_points, noise=0):
-    theta = np.linspace(0, 10*np.pi, num_points)
+def generate_spiral(num_points, theta_max=2, noise=0):
+    theta = np.linspace(0, theta_max*np.pi, num_points)
 
     x = theta * np.cos(theta) + np.random.uniform(-noise, noise, num_points)
     y = theta * np.sin(theta) + np.random.uniform(-noise, noise, num_points)
     return x, y
 
 # Number of points in each spiral
-num_points = 300
+num_points = 250
 pi_symbol = '\u03c0'
+for theta_max in (2, 4, 5, 10, 12):
+    new_folder = f'{main_dir}/theta_max={theta_max}'
+    os.makedirs(new_folder)
+    for max_noise in (0, 0.2, 0.4, 0.6, 0.8, 1, 1.2):
+        new_sub_folder = f'{new_folder}/max_noise={max_noise}'
+        os.makedirs(new_sub_folder)
+        # Generate data for two spirals with noise
+        x1, y1 = generate_spiral(num_points, theta_max=theta_max, noise=max_noise)
+        # x2, y2 = generate_spiral(num_points, noise=0.1)
+        x2, y2 = -x1, -y1
+        # Plot the spirals with noise
+        plt.figure(figsize=(8, 6))
+        # plt.style.reload_library()
+        # plt.style.use('science')
+        plt.scatter(x1, y1, label='Spiral 1', color='blue')
+        plt.scatter(x2, y2, label='Spiral 2', color='red')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title(f'Theta max={theta_max}{pi_symbol} _ Max Noise = {max_noise}')
 
-for max_noise in (0, 0.2, 0.4, 0.6, 0.8, 1):
-    # Generate data for two spirals with noise
-    x1, y1 = generate_spiral(num_points, noise=max_noise)
-    # x2, y2 = generate_spiral(num_points, noise=0.1)
-    x2, y2 = -x1, -y1
-    # Plot the spirals with noise
-    plt.figure(figsize=(8, 6))
-    plt.scatter(x1, y1, label='Spiral 1', color='blue')
-    plt.scatter(x2, y2, label='Spiral 2', color='red')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title(f'Theta max=10{pi_symbol} _ Max Noise = {max_noise}')
+        plt.xlabel('X', fontsize=24)
+        plt.ylabel('Y', fontsize=24)
+        plt.gca().set_aspect('equal')
 
-    plt.xlabel('X', fontsize=24)
-    plt.ylabel('Y', fontsize=24)
-    #plt.title(f'Theta_max = {cnt}{pi_symbol}', fontsize=36)
-    plt.gca().set_aspect('equal')
+        # Add a legend
+        plt.legend(fontsize=14)
+        #plt.show()
+        #
+        #
+        # #dffffff
+        plt.savefig(f'{new_sub_folder}/Spiral_theta_max={theta_max}pi_max_noise={max_noise}.png')
+        plt.close()
+        # # cnt += 1
+        # # Show the plot
 
-    # Add a legend
-    plt.legend(fontsize=14)
-    #plt.show()
+        combined_array1 = np.column_stack((x1.flatten(), y1.flatten()))
+        combined_array2 = np.column_stack((x2.flatten(), y2.flatten()))
 
+        # np.savetxt('Spiraldata.txt', np.hstack(combined_array1.flatten(), combined_array2.flatten()), delimiter='\t')
 
-    #dffffff
-    plt.savefig(f'{save_dir}/Spiral_theta_max=10pi_max_noise={max_noise}.png')
-    plt.close()
-    # cnt += 1
-    # Show the plot
-
-    combined_array1 = np.column_stack((x1.flatten(), y1.flatten()))
-    combined_array2 = np.column_stack((x2.flatten(), y2.flatten()))
-
-    # np.savetxt('Spiraldata.txt', np.hstack(combined_array1.flatten(), combined_array2.flatten()), delimiter='\t')
-
-    with open(f'{save_dir}/Spiraldata_theta_max=10pi_max_noise={max_noise}.txt', 'w') as file:
-        for x in combined_array1:
-            file.write(f'1,{np.round(x[0],3)},{np.round(x[1],3)}\n')
-        for y in combined_array2:
-            file.write(f'0,{np.round(y[0], 3)},{np.round(y[1], 3)}\n')
+        with open(f'{new_sub_folder}/Spiraldata_theta_max={theta_max}pi_max_noise={max_noise}.txt', 'w') as file:
+            for x in combined_array1:
+                file.write(f'1,{np.round(x[0],3)},{np.round(x[1],3)}\n')
+            for y in combined_array2:
+                file.write(f'0,{np.round(y[0], 3)},{np.round(y[1], 3)}\n')
